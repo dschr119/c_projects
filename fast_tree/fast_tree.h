b08e77ef19d451235d81c3ee2786d6e3d5154597
dschroeder205 <dschroeder205@gmail.com>
2021-07-30 21:38:25 -0500

    file creation, class definition

---
+++ fast_tree/fast_tree.h
@@ -0,0 +1,16 @@
+#include <vector>
+
+template <class Type>
+class FastTree{
+
+public:
+
+  void insert( Type t_data );
+  void remove( Type t_data );
+
+private:
+
+  std::vector<Type>
+
+
+};
---
+++ fast_tree/main.cpp
@@ -0,0 +1,6 @@
+#include "fast_tree.h"
+#include <iostream>
+
+int main(){
+  std::cout << "ran" << std::endl;
+}
